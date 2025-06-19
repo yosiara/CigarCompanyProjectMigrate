454ac5a5ebed2fd38cfc5e811527a32b2faffd5c
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2025-06-19 08:36:56 -0400

    feat: init commit

--- extra-addons/l10n_cu_report_docxtpl/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import docxtpl_formats
-from . import ir_actions_report_xml
-from . import docxtpl_template
-from . import report
-from . import docxtpl_report
--- extra-addons/smoke/report/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import smoke_report
