5c187133df0067a53894e6d63cd84bcbf72feddd
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2025-08-16 18:04:00 -0400

    feat: form button save and cancel modified

---
+++ extra-addons/web_community/static/src/views/form/form_status_indicator/form_status_indicator.js
@@ -0,0 +1,4 @@
+/** @odoo-module **/
+import { FormStatusIndicator } from "@web/views/form/form_status_indicator/form_status_indicator";
+
+FormStatusIndicator.template = "web_community.FormStatusIndicator";
