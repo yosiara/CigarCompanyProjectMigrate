14798b254f3bbb3100fcfba343ae71295293f161
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2025-12-27 13:54:02 -0500

    feat: override sync_user in employee

--- extra-addons/hr_turei/models/hr_employee.py
+++ extra-addons/hr_turei/models/hr_employee.py
@@ -18,20 +18,34 @@ class HREmployee(models.Model):
             return False # comodidad para filtros posteriores
         return document.datas # Obtener el contenido binario de la imagen
 
-    def action_assign_photo_from_documents(self):
+    def action_assign_photo_and_sync_user(self):
         """Acción para asignar foto a todos los empleados desde documentos"""
         employees = self.search([])
 
-        # Insertar foto para todos los empleados con codigo de trabajador
-        count = 0
+        count_images = 0
+        count_sync_user = 0
         for employee in employees:
+            # Buscar e insertar foto para empleado con código de trabajador
             image_1920 = self._get_photo_from_documents(registration_number=employee.registration_number)
             if image_1920 != employee.image_1920:
                 employee.image_1920 = image_1920
-                _logger.info(f"----------->> Foto asignada a: {employee.name}")
-                count += 1
+                _logger.info(f"----------->> Foto asignada al empleado: {employee.name}")
+                count_images += 1
+            
+            # Buscar matching con usuario, syncronizar y vincular
+            username = (employee.work_email).split('@')[0]
+            user = self.env['res.users'].search([('login', '=', username), ('share', '=', False)], limit=1)
+            if user and not employee.user_id:
+                employee_data = user.get_sync_employee_data(employee)
+                user.write(**employee_data)
+                employee.user_id = user.id
+                _logger.info(f"----------->> Usuario con ID = {user.id} sincronizado y vinculado a {employee.name}")
+                count_sync_user += 1
 
-        message = f"----------->> Asignadas {count} fotos correctamente desde documentos!!!"
+
+        message = (f"----------->> Asignadas {count_images} fotos"
+                  f"------------>> Sincronizados {count_sync_user} usuarios"
+        )
         _logger.info(message)
         return {
             'type': 'ir.actions.client',
@@ -39,13 +53,41 @@ class HREmployee(models.Model):
             'params': {
                 'type': 'success',
                 'message': message,
-                'next': {'type': 'ir.actions.act_window_close'},
+                # 'next': {'type': 'ir.actions.act_window_close'},
             }
         }
         
     # ------------------------------------------------------------------------ #
     #                           OVERRIDE METHODS                               #
     # ------------------------------------------------------------------------ #
+
+    def _sync_user(self, user, employee_has_image=False):
+        sync_vals = {}
+
+        if self.name != user.name:
+            sync_vals['name'] = self.name
+        if employee_has_image and self.image_1920 != user.image_1920:
+            sync_vals['image_1920'] = self.image_1920
+        if self.work_phone != user.phone:
+            sync_vals['phone'] = self.work_phone
+        if self.mobile_phone != user.mobile:
+            sync_vals['mobile'] = self.mobile_phone
+        if self.work_email != user.email:
+            sync_vals['email'] = self.work_email
+        if self.job_id and self.job_id.name != user.function:
+            sync_vals['function'] = self.job_id.name
+        if sync_vals:
+            user.write(**sync_vals)
+        
+        vals = dict(
+            work_contact_id=user.partner_id.id if user else self.work_contact_id.id,
+            user_id=user.id,
+        )
+        # if not employee_has_image:
+        #     vals['image_1920'] = user.image_1920
+        if user.tz:
+            vals['tz'] = user.tz
+        return vals
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -60,22 +102,22 @@ class HREmployee(models.Model):
     #                           ONCHANGE METHODS                               #
     # ------------------------------------------------------------------------ #
 
-    @api.onchange('name', 'user_id')
+    @api.onchange('name')
     def _onchange_name(self):
         if self.name and self.user_id and self.name != self.user_id.name:
             self.user_id.name = self.name
 
-    @api.onchange('work_phone', 'user_id')
+    @api.onchange('work_phone')
     def _onchange_work_phone(self):
         if self.work_phone and self.user_id and self.work_phone != self.user_id.phone:
             self.user_id.phone = self.work_phone
 
-    @api.onchange('image_1920', 'user_id')
+    @api.onchange('image_1920')
     def _onchange_image_1920(self):
         if self.image_1920 and self.user_id and self.image_1920 != self.user_id.image_1920:
             self.user_id.image_1920 = self.image_1920
 
-    @api.onchange('job_id', 'user_id')
+    @api.onchange('job_id')
     def _onchange_job_id(self):
         if self.job_id and self.user_id and self.job_id.name != self.user_id.function:
             self.user_id.function = self.job_id.name
--- extra-addons/hr_turei/models/res_users.py
+++ extra-addons/hr_turei/models/res_users.py
@@ -6,9 +6,10 @@ _logger = logging.getLogger(__name__)
 class ResUsersExtended(models.Model):
     _inherit = 'res.users'
     
-    def _find_matching_employee(self, login):
+    def _find_matching_employee(self):
         """Buscar empleado existente para este usuario"""
-        username = login.split('@')[0].lower() if '@' in login else login
+        self.ensure_one()
+        username = (self.login).split('@')[0].lower() if '@' in self.login else self.login
         
         # 2. Buscar por username@dominio
         employee = self.env['hr.employee'].search([
@@ -18,47 +19,59 @@ class ResUsersExtended(models.Model):
 
         return employee or False
 
-    def _sync_employee(self, employee):
-        """Devuelve valores para sincronizar desde empleado a usuario"""
-        return {
-            # 'create_employee_id': employee.id,
-            'name': employee.name,
-            'image_1920': employee.image_1920,
-            'phone': employee.work_phone,
-            'mobile': employee.mobile_phone,
-            'email': employee.work_email,
-            'function': employee.job_id.name,
-            'partner_id': employee.work_contact_id.id,
-            # 'login': (employee.work_email).split('@')[0].lower(),
-        }
+    # def get_sync_employee_data(self, employee):
+    #     """Devuelve valores para sincronizar desde empleado a usuario"""
+    #     return {
+    #         # 'create_employee_id': employee.id,
+    #         'name': employee.name,
+    #         'image_1920': employee.image_1920,
+    #         'phone': employee.work_phone,
+    #         'mobile': employee.mobile_phone,
+    #         'email': employee.work_email,
+    #         'function': employee.job_id.name,
+    #         # 'partner_id': employee.work_contact_id.id,
+    #         # 'login': (employee.work_email).split('@')[0].lower(),
+    #     }
 
     # ------------------------------------------------------------------------ #
     #                           OVERRIDE METHODS                               #
     # ------------------------------------------------------------------------ #
 
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     """Cuando se crea usuario, sincronizar datos desde empleado existente"""
+    #     employee_by_login = {}
+        
+    #     for vals in vals_list:
+    #         # Buscar empleado que coincida por login (email)
+    #         login = vals.get('login')
+    #         if login:
+    #             employee = self._find_matching_employee(login)
+    #             if employee:
+    #                 # Sincronizar con empleado
+    #                 employee_data = self.get_sync_employee_data(employee)
+    #                 vals.update(**employee_data)
+        
+    #                 # Guardar referencia para vincular después
+    #                 employee_by_login[login] = employee
+
+    #     users = super(ResUsersExtended, self).create(vals_list)
+        
+    #     # Vincular empleados
+    #     for user in users:
+    #         employee_by_login[user.login].write({'user_id': user.id})
+        
+    #     return users
+
     @api.model_create_multi
     def create(self, vals_list):
-        """Cuando se crea usuario, sincronizar datos desde empleado existente"""
-        employee_by_login = {}
-        
-        for vals in vals_list:
-            # Buscar empleado que coincida por login (email)
-            login = vals.get('login')
-            if login:
-                employee = self._find_matching_employee(login)
-                if employee:
-                    # Sincronizar con empleado
-                    sync_employee = self._sync_employee(employee)
-                    vals.update(**sync_employee)
-        
-                    # Guardar referencia para vincular después
-                    employee_by_login[login] = employee
-
+        """Cuando se crea usuario, sincronizar empleado existente"""
         users = super(ResUsersExtended, self).create(vals_list)
         
         # Vincular empleados
         for user in users:
-            employee_by_login[user.login].write({'user_id': user.id})
+            employee = user._find_matching_employee()
+            employee.write({'user_id': user.id})
         
         return users
         
