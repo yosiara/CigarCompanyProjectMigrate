31eb0500dfc7a8e9adcdb106164654637a78bf9e
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2025-12-28 20:22:47 -0500

    feat: sync user from employee

--- extra-addons/hr_turei/models/hr_employee.py
+++ extra-addons/hr_turei/models/hr_employee.py
@@ -66,38 +66,22 @@ class HREmployee(models.Model):
         #     self.name = self.user_id.name
         pass
 
-    def _sync_user(self, user, employee_has_image=False):
-        sync_vals = {}
-
-        # Sincronizar usuario, priorizar la siguiente información del empleado, antes del vínculo
-        if self.name != user.name:
-            sync_vals['name'] = self.name
-        if employee_has_image and self.image_1920 != user.image_1920:
-            sync_vals['image_1920'] = self.image_1920
-        if self.work_phone != user.phone:
-            sync_vals['phone'] = self.work_phone
-        if self.mobile_phone != user.mobile:
-            sync_vals['mobile'] = self.mobile_phone
-        if self.work_email != user.email:
-            sync_vals['email'] = self.work_email
-        if self.job_id and self.job_id.name != user.function:
-            sync_vals['function'] = self.job_id.name
-        if sync_vals:
-            user.write(**sync_vals)
-        
-        vals = dict(
-            work_contact_id=user.partner_id.id if user else self.work_contact_id.id,
-            user_id=user.id,
-        )
-        # if not employee_has_image:
-        #     vals['image_1920'] = user.image_1920
-        if user.tz:
-            vals['tz'] = user.tz
-        return vals
-
     # def _sync_user(self, user, employee_has_image=False):
-    #     """Sincronizar usuario, priorizar información del empleado, antes del vínculo"""
-    #     sync_vals = user._sync_employee(employee=self, employee_has_image=employee_has_image)
+    #     sync_vals = {}
+
+    #     # Sincronizar usuario, priorizar la siguiente información del empleado, antes del vínculo
+    #     if self.name != user.name:
+    #         sync_vals['name'] = self.name
+    #     if employee_has_image and self.image_1920 != user.image_1920:
+    #         sync_vals['image_1920'] = self.image_1920
+    #     if self.work_phone != user.phone:
+    #         sync_vals['phone'] = self.work_phone
+    #     if self.mobile_phone != user.mobile:
+    #         sync_vals['mobile'] = self.mobile_phone
+    #     if self.work_email != user.email:
+    #         sync_vals['email'] = self.work_email
+    #     if self.job_id and self.job_id.name != user.function:
+    #         sync_vals['function'] = self.job_id.name
     #     if sync_vals:
     #         user.write(**sync_vals)
         
@@ -110,6 +94,22 @@ class HREmployee(models.Model):
     #     if user.tz:
     #         vals['tz'] = user.tz
     #     return vals
+
+    def _sync_user(self, user, employee_has_image=False):
+        """Sincronizar usuario antes del vínculo, priorizar información del empleado"""
+        vals = user._sync_employee(employee=self, employee_has_image=employee_has_image)
+        if vals:
+            user.write(**vals)
+        
+        vals = dict(
+            work_contact_id=user.partner_id.id if user else self.work_contact_id.id,
+            user_id=user.id,
+        )
+        # if not employee_has_image:
+        #     vals['image_1920'] = user.image_1920
+        if user.tz:
+            vals['tz'] = user.tz
+        return vals
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -124,28 +124,39 @@ class HREmployee(models.Model):
     #                           ONCHANGE METHODS                               #
     # ------------------------------------------------------------------------ #
 
-    @api.onchange('name')
-    def _onchange_name(self):
-        if self.name and self.user_id and self.name != self.user_id.name:
-            self.user_id.name = self.name
-
-    @api.onchange('work_phone')
-    def _onchange_work_phone(self):
-        if self.work_phone and self.user_id and self.work_phone != self.user_id.phone:
-            self.user_id.phone = self.work_phone
-
-    @api.onchange('image_1920')
-    def _onchange_image_1920(self):
-        if self.image_1920 and self.user_id and self.image_1920 != self.user_id.image_1920:
-            self.user_id.image_1920 = self.image_1920
-
-    @api.onchange('job_id')
-    def _onchange_job_id(self):
-        if self.job_id and self.user_id and self.job_id.name != self.user_id.function:
-            self.user_id.function = self.job_id.name
-
     @api.onchange('registration_number')
     def _onchange_registration_number(self):
         image_1920 = self._get_photo_from_documents(registration_number=self.registration_number)
         if image_1920 != self.image_1920:
             self.image_1920 = image_1920
+
+    """ Sincronizar datos del empleado al usuario """
+    @api.onchange('name')
+    def _onchange_name(self):
+        # 1. Nombre (empleado → usuario)
+        if self.name and self.user_id and self.name != self.user_id.name:
+            self.user_id.name = self.name
+
+    @api.onchange('image_1920')
+    def _onchange_image_1920(self):
+        # 2. Imagen (empleado → usuario)
+        if self.image_1920 and self.user_id and self.image_1920 != self.user_id.image_1920:
+            self.user_id.image_1920 = self.image_1920
+    
+    @api.onchange('work_phone')
+    def _onchange_work_phone(self):
+        # 3. Work Phone (empleado → usuario)
+        if self.work_phone and self.user_id and self.work_phone != self.user_id.phone:
+            self.user_id.phone = self.work_phone
+
+    @api.onchange('job_id')
+    def _onchange_job_id(self):
+        # 4. Job (empleado → usuario)
+        if self.job_id and self.user_id and self.job_id.name != self.user_id.function:
+            self.user_id.function = self.job_id.name
+
+    @api.onchange('active')
+    def _onchange_job_id(self):
+        # 5. Active (empleado → usuario)
+        if self.user_id and not self.user_id._is_admin() and self.active != self.user_id.active:
+            self.user_id.active = self.active
--- extra-addons/hr_turei/models/res_users.py
+++ extra-addons/hr_turei/models/res_users.py
@@ -24,42 +24,45 @@ class ResUsersExtended(models.Model):
 
         return employee or False
 
-    # def _sync_employee(self, employee, employee_has_image=False):
-    #     """Sincronizar datos del empleado al usuario"""
-    #     self.ensure_one()
+    def _sync_employee(self, employee, employee_has_image=False):
+        """Sincronizar datos del empleado al usuario"""
+        self.ensure_one()
         
-    #     sync_vals = {}
+        sync_vals = {}
         
-    #     # 1. Nombre (empleado → usuario)
-    #     if employee.name and employee.name != self.name:
-    #         sync_vals['name'] = employee.name
+        # 1. Nombre (empleado → usuario)
+        if employee.name and employee.name != self.name:
+            sync_vals['name'] = employee.name
         
-    #     # 2. Imagen (empleado → usuario)
-    #     if employee_has_image and employee.image_1920 != self.image_1920:
-    #         sync_vals['image_1920'] = employee.image_1920
+        # 2. Imagen (empleado → usuario)
+        if employee_has_image and employee.image_1920 != self.image_1920:
+            sync_vals['image_1920'] = employee.image_1920
 
-    #     # 3. Work Phone (empleado → usuario)
-    #     if employee.work_phone != self.phone:
-    #         sync_vals['phone'] = employee.work_phone
+        # 3. Work Phone (empleado → usuario)
+        if employee.work_phone != self.phone:
+            sync_vals['phone'] = employee.work_phone
 
-    #     # 4. Mobile Phone (empleado → usuario)
-    #     if employee.mobile_phone != self.mobile:
-    #         sync_vals['mobile'] = employee.mobile_phone
+        # 4. Mobile Phone (empleado → usuario)
+        if employee.mobile_phone != self.mobile:
+            sync_vals['mobile'] = employee.mobile_phone
         
-    #     # 5. Work Email (empleado → usuario)
-    #     if employee.work_email and employee.work_email != self.email:
-    #         sync_vals['email'] = employee.work_email
+        # 5. Work Email (empleado → usuario)
+        if employee.work_email and employee.work_email != self.email:
+            # username = (self.email).split('@')[0].lower()
+            # sync_vals['login'] = username
+            sync_vals['email'] = employee.work_email
 
-    #     # 6. Job (empleado → usuario)
-    #     if employee.job_id and employee.job_id.name != self.function:
-    #         sync_vals['function'] = employee.job_id.name
+
+        # 6. Job (empleado → usuario)
+        if employee.job_id and employee.job_id.name != self.function:
+            sync_vals['function'] = employee.job_id.name
         
-    #     # 7. Archivar/Desarchivar usuario asociado, excluyendo administrador del sistema
-    #     if employee.active != self.active and not self._is_admin():
-    #         sync_vals['active'] = employee.active
+        # 7. Active (empleado → usuario)
+        if employee.active != self.active and not self._is_admin():
+            sync_vals['active'] = employee.active
 
-    #     # Retornar valores
-    #     return sync_vals
+        # Retornar valores
+        return sync_vals
 
     # def get_sync_employee_data(self, employee):
     #     """Devuelve valores para sincronizar desde empleado a usuario"""
