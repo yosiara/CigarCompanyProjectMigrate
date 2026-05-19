d06b1a4c28b366e53ba3cdbe30d54ccf6ba77f62
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2026-05-19 18:40:05 -0400

    feat: employee models with services

--- extra-addons/ict/models/ict_employee.py
+++ extra-addons/ict/models/ict_employee.py
@@ -12,29 +12,34 @@ class ICTEmployee(models.Model):
     _description = 'Domain Employees'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    employee_id = fields.Many2one(comodel_name='hr.employee', string='ICT Employee', required=True, tracking=True)
-    name = fields.Char(string='Name', related='employee_id.name')
-    department_id = fields.Many2one(string='Department', related='employee_id.department_id')
-    user_id = fields.Many2one(related='employee_id.user_id')
-    
-    work_email = fields.Char(related='employee_id.work_email', readonly=False, related_sudo=False)
-    work_phone = fields.Char(related='employee_id.work_phone', readonly=False, related_sudo=False)
-    mobile_phone = fields.Char(related='employee_id.mobile_phone', readonly=False, related_sudo=False)
-    domain = fields.Char(string='Domain', required=True, tracking=True)
-    domain_user = fields.Char(string='Domain Username', required=True, tracking=True)
-    position = fields.Char(string='Position')
+    domain_user = fields.Char(string='Username *', required=True, tracking=True)
+    domain_id = fields.Many2one(string='Domain', comodel_name='mail.alias.domain', default=lambda self: self.env.company.alias_domain_id.id)
+    work_email = fields.Char(string='Email', compute='_compute_work_email', readonly=True)
     hire_date = fields.Date(string='Hire Date')
     active = fields.Boolean(default=True)
     
-    # Relations
     computer_ids = fields.One2many('ict.computer', 'employee_ids', string='Assigned Computers')
     phone_ids = fields.One2many('ict.phone', 'employee_id', string='Assigned Phones')
     
-    @api.depends('domain', 'domain_user')
-    def _compute_email(self):
+    # Related employee fields
+    employee_id = fields.Many2one(comodel_name='hr.employee', string='Employee *', required=True, tracking=True)
+    name = fields.Char(related='employee_id.name')
+    user_id = fields.Many2one(related='employee_id.user_id')
+    work_phone = fields.Char(related='employee_id.work_phone', readonly=False, related_sudo=False)
+    mobile_phone = fields.Char(related='employee_id.mobile_phone', readonly=False, related_sudo=False)
+    job_title = fields.Char(related='employee_id.job_title')
+    department_id = fields.Many2one(related='employee_id.department_id')
+    
+    @api.depends('domain_id', 'domain_user')
+    def _compute_work_email(self):
         for employee in self:
-            if employee.domain and employee.domain_user:
-                employee.work_email = employee.domain_user + '@' + employee.domain
+            if employee.domain_id and employee.domain_user:
+                employee.work_email = employee.domain_user + '@' + employee.domain_id.name
+            else:
+                employee.work_email = False
+            employee.employee_id.sudo().write({
+                'work_email': employee.work_email
+            })
 
     @api.constrains('work_email')
     def _check_email(self):
