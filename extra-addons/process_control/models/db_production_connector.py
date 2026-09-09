0c5d0627bbed01b2ab159c2af5fe5c5e473d26c1
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2026-09-09 09:07:55 -0400

    fix: db_production_connector

--- extra-addons/process_control/models/db_production_connector.py
+++ extra-addons/process_control/models/db_production_connector.py
@@ -2,8 +2,8 @@
 
 import psycopg2
 import socket
-from odoo import models, fields, api, tools
-from odoo.exceptions import Warning
+from odoo import models, fields
+from odoo.exceptions import UserError, Warning
 
 CONNECTORS = [('postgresql', 'PostgreSQL')]
 
@@ -16,24 +16,37 @@ class DBProductionConnector(models.Model):
     name = fields.Char(string='Datasource Name', required=True, default='DB Sistema de producción')
     server = fields.Char(string="Server", required=True, default='localhost')
     port = fields.Integer(string="Port", required=True, default=5432)
-    user = fields.Char(string="User", required=True, default='odoov10')
+    user = fields.Char(string="User", required=True, default='odoo18')
     pwd = fields.Char(string="Password", required=True)
     dbname = fields.Char(string="Database Name", required=True)
     connector = fields.Selection(CONNECTORS, 'Connector', required=True, default='postgresql')
 
-    
     def action_test_connection(self):
         if self.connector == 'postgresql':
             try:
                 if self.checkServerListen():
                     conn = psycopg2.connect(
-                        database=self.dbname, user=self.user, password=self.pwd, port=self.port, host=self.server)
+                        database=self.dbname,
+                        user=self.user,
+                        password=self.pwd,
+                        port=self.port,
+                        host=self.server
+                    )
                     conn.close()
                 else:
-                    raise Exception(u'imposible conectar con el servidor postgres.' + self.server)
-            except Exception, e:
-                raise Warning(u'Conexión Fallida:' + tools.ustr(e.message))
-            raise Warning(u'Conexión ok')
+                    raise Exception('No se pudo conectar al servidor PostgreSQL en {}:{}'.format(self.server, self.port))
+            except Exception as e:
+                raise UserError('Conexión Fallida: {}'.format(str(e)))
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Conexión exitosa',
+                    'message': 'La conexión a PostgreSQL ha sido exitosa.',
+                    'type': 'success',
+                    'sticky': False,
+                }
+            }
         return True
 
     def connect(self):
@@ -41,16 +54,26 @@ class DBProductionConnector(models.Model):
             try:
                 if self.checkServerListen():
                     self._connection = psycopg2.connect(
-                        database=self.dbname, user=self.user, password=self.pwd, port=self.port, host=self.server)
+                        database=self.dbname,
+                        user=self.user,
+                        password=self.pwd,
+                        port=self.port,
+                        host=self.server
+                    )
                     return self._connection
-                return False
-            except Exception, e:
-                raise Warning(u'Conexión Fallida:' + tools.ustr(e.message))
+                else:
+                    raise Exception('No se pudo conectar al servidor PostgreSQL en {}:{}'.format(self.server, self.port))
+            except Exception as e:
+                raise UserError('Conexión Fallida al abrir la conexión: {}'.format(str(e)))
         return False
 
     def close(self):
         if self._connection:
-            self._connection.close()
+            try:
+                self._connection.close()
+            except Exception as e:
+                pass
+            self._connection = False
         return True
 
     def checkServerListen(self):
@@ -59,6 +82,6 @@ class DBProductionConnector(models.Model):
         try:
             s.connect((self.server, self.port))
             s.close()
-        except socket.error, ex:
+            return True
+        except socket.error as ex:
             return False
-        return True
