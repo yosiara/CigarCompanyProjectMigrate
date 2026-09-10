ce42dcf3ddd993448e4fd4d6575d03df0e2b3ea1
Yosiel Arango Arencibia <arangoyosiel@gmail.com>
2026-09-09 22:23:44 -0400

    translate: use English for consistency

--- extra-addons/process_control/wizard/compliance_planned_cdt_wzd.py
+++ extra-addons/process_control/wizard/compliance_planned_cdt_wzd.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 import json
 from odoo import models, fields
-from odoo.tools import date_utils
 
 class WzdCompliancePlannedCdtToExcel(models.TransientModel):
     _name = 'process_control.compliance_planned_cdt_excel_wzd'
     _description = 'Compliance Planned CDT Report Wzd'
 
-    start_date = fields.Date('Desde', required=True)
-    end_date = fields.Date('Hasta', required=True)
+    start_date = fields.Date('Start Date *', required=True)
+    end_date = fields.Date('End Date *', required=True)
 
     def export_to_xlsx(self):
         data = {
-                'start_date': self.start_date,
-                'end_date': self.end_date,
+                'start_date': fields.Date.to_string(self.start_date),
+                'end_date': fields.Date.to_string(self.end_date),
             }
         return {
             'type': 'ir.actions.report',
             'data': {'model': 'report.process_control.compliance_planned_cdt',
-                     'options': json.dumps(data, default=date_utils.json_default),
+                     'options': json.dumps(data),
                      'output_format': 'xlsx',
-                     'report_name': 'Compliance Planned CDT Excel Report',
+                     'report_name': f'Compliance Planned CDT Excel Report ({self.start_date}-{self.end_date})',
                      },
             'report_type': 'xlsx',
         }
